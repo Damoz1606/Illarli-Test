09089af407fd60fd022e302bcc582ce3e3fe67a0
Damoz1606 <panchitodmun@gmail.com>
2023-03-17 10:29:30 -0500

    :sparkles: Create Custom Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Error404 from 'pages/Error404';
+import Login from 'pages/Login';
+import Home from 'pages/Home';
+import LoginLayout from 'layouts/LoginLayout';
+import HomeLayout from 'layouts/HomeLayout';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +15,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<Error404 />} />
+        <Route path="" element={<Navigate to="login" />} />
+        <Route path="login" element={<LoginLayout />} >
+          <Route index element={<Login />} />
+        </Route>
+        <Route path="home" element={<HomeLayout />} >
+          <Route index element={<Home />} />
+        </Route>
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const HomeLayout = () => {
   return (
-    <div>HomeLayout</div>
+    <Outlet />
   )
 }
 
